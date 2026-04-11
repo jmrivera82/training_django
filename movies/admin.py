26862bace8c5cd3c1d32a16ebe9d42ff65f5e8a6
jmrivera82 <jmrivera82@gmail.com>
2026-04-11 09:00:43 -0400

    feat: Agrega admin y modelo Movie

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from movies.models import Movie
 
 # Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+
+    list_display=('title','duration')
+
+
+admin.site.register(Movie, MovieAdmin)
